dad93a89dbde550f6811092154bf5bb15e9ef696
Kinshu Jain <76443618+kinnshuuu@users.noreply.github.com>
2022-11-27 02:44:48 +0530

    Time: 156 ms (10.10%), Space: 77.7 MB (7.36%) - LeetHub

---
+++ 0980-unique-paths-iii/0980-unique-paths-iii.cpp
@@ -0,0 +1,47 @@
+class Solution
+{
+    public:
+
+        int dfs(vector<vector < int>> arr, int x, int y, int zero)
+        {
+            int n = arr.size();
+            int m = arr[0].size();
+
+            if (x < 0 || y < 0 || x >= n || y >= m || arr[x][y] == -1)
+                return 0;
+
+            if (arr[x][y] == 2)
+                return zero == -1 ? 1 : 0;
+
+            arr[x][y] = -1;
+            zero--;
+
+            int ans = dfs(arr, x + 1, y, zero) +
+                dfs(arr, x, y + 1, zero) +
+                dfs(arr, x - 1, y, zero) +
+                dfs(arr, x, y - 1, zero);
+
+            return ans;
+        }
+    int uniquePathsIII(vector<vector < int>> &arr)
+    {
+        int n = arr.size();
+        int m = arr[0].size();
+        int x, y, zero = 0;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (arr[i][j] == 0)
+                    zero++;
+                else if (arr[i][j] == 1)
+                {
+                    x = i;
+                    y = j;
+                }
+            }
+        }
+
+        return dfs(arr, x, y, zero);
+    }
+};
