e4cfe1debaa5c4a596ae614b26ff83abfb2bd7a0
Kinshu Jain <76443618+kinnshuuu@users.noreply.github.com>
2023-01-21 16:55:31 +0530

    Time: 2 ms (53.26%), Space: 7.3 MB (40.93%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.cpp
+++ 0140-word-break-ii/0140-word-break-ii.cpp
@@ -1,24 +1,39 @@
 class Solution
 {
     public:
-    set<string> st;
+        set<string> st;
 
-    map<int, vector<string>> mp;
+    map<int, vector < string>> mp;
     vector<string> solve(string &s, int i)
     {
+
         vector<string> ans;
-        for(int j = i; j < s.size(); j++) {
-            if(st.count(s.substr(i, j-i+1))) {
-                if(j == s.size() - 1) {
-                    ans.push_back(s.substr(i, j-i+1));
+        for (int j = i; j < s.size(); j++)
+        {
+
+            if (st.count(s.substr(i, j - i + 1)))
+            {
+                if (j == s.size() - 1)
+                {
+                    ans.push_back(s.substr(i, j - i + 1));
                     break;
                 }
-                auto vec = solve(s, j+1);
-                for(auto t: vec) {
-                    ans.push_back(s.substr(i, j-i+1) + " " + t);
+                if (mp.find(j+1) != mp.end())
+                {
+                    for (auto t: mp[j+1])
+                    {
+                        ans.push_back(s.substr(i, j - i + 1) + " " + t);
+                    }
+                    continue;
+                }
+                auto vec = solve(s, j + 1);
+                for (auto t: vec)
+                {
+                    ans.push_back(s.substr(i, j - i + 1) + " " + t);
                 }
             }
         }
+        mp[i] = ans;
         return ans;
     }
     vector<string> wordBreak(string s, vector<string> &wordDict)
