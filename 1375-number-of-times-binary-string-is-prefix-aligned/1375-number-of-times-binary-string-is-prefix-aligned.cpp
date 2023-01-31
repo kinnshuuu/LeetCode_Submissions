4368c248e44aa07c36ade812ca0a4dc37ef7a8f0
Kinshu Jain <76443618+kinnshuuu@users.noreply.github.com>
2023-01-31 21:05:48 +0530

    Time: 77 ms (56.89%), Space: 38.5 MB (68.55%) - LeetHub

--- 1375-number-of-times-binary-string-is-prefix-aligned/1375-number-of-times-binary-string-is-prefix-aligned.cpp
+++ 1375-number-of-times-binary-string-is-prefix-aligned/1375-number-of-times-binary-string-is-prefix-aligned.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     int numTimesAllBlue(vector<int>& arr) {
+        int ans = 0;
         int n = arr.size();
-        vector<long long> brr(n,0);
-        brr[0] = arr[0];
+        int l = arr[0];
         
-        for(int i = 1; i<n; i++)
+        for(int i = 0; i<n; i++)
         {
-            brr[i] += arr[i]+ brr[i-1];
-        }
-        long long ans = 0;
-        for(long long i = 0; i<n; i++)
-        {
-            if(brr[i] == ((i+1)*(i+2))/2)
+            l = max(l,arr[i]);
+            if(l == i+1)
             {
                 ans++;
             }
+            
         }
+        
         return ans;
     }
 };
